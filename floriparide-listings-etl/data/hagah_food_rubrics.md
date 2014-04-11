28dc89d414b3980b2179f94b39cbe5ff441c4d83
mikhail.bragin <mikhail@grupoxago.com>
2014-04-11 15:43:21 -0300

    added some hagah data

---
+++ floriparide-listings-etl/data/hagah_food_rubrics.md
@@ -0,0 +1,52 @@
+00000001 Açougues
+00000002 Alimentos Congelados
+00000003 Alimentos Dietéticos
+00000004 Alimentos Orgânicos
+00000005 Alimentos Prontos
+00000006 Alimentos
+00000007 Artigos e equipamentos para churrasco
+00000008 Artigos para bebês
+00000009 Bares
+00000010 Bebidas
+00000011 Bistrô
+00000012 Bufê
+00000013 Cafés e casas de chá
+00000014 Cestas de alimentos
+00000015 Cestas de presente
+00000016 Churrascaria
+00000017 Cozinha árabe
+00000018 Cozinha chinesa
+00000019 Cozinha internacional
+00000020 Cozinha italiana
+00000021 Cozinha japonesa
+00000022 Cozinha regional
+00000023 Cozinha variada
+00000024 Cozinha vegetariana
+00000025 Distribuidoras de produtos alimentícios
+00000026 Doces
+00000027 Esporte
+00000028 Eventos
+00000029 Fast-food e Lancheria
+00000030 Feijoada
+00000031 Frutos do mar
+00000032 Gaúcha
+00000033 Grelhados
+00000034 Lanchonetes, Casas de Chá e de Sucos
+00000035 Lojas
+00000036 Mate
+00000037 Mercearias
+00000038 Natural e light
+00000039 Padarias e confeitarias
+00000040 Padarias
+00000041 Peixarias
+00000042 Pizzarias
+00000043 Postos de Combustível
+00000044 Produtos naturais
+00000045 Pubs
+00000046 Restaurantes
+00000047 Salgadinhos
+00000048 Sopas
+00000049 Sorveterias
+00000050 Supermercados
+00000051 Suplementos alimentares
+00000052 Transportes e Veículos
--- floriparide-listings-etl/data/hagah_usefull_queries_raw_data.sql
+++ floriparide-listings-etl/data/hagah_usefull_queries_raw_data.sql
@@ -1,4 +1,4 @@
-#regex regexp_replace(element::text, '(\(.*\))','') to clean up facilities from shit like here "Tele-entrega(com pedido mínimo de R$10)", we need only Tele-entrega
+--regex regexp_replace(element::text, '(\(.*\))','') to clean up facilities from shit like here "Tele-entrega(com pedido mínimo de R$10)", we need only Tele-entrega
 SELECT regexp_replace(element::text, '(\(.*\))','')
 	FROM raw_data.data AS d,
 	json_array_elements(d.data->'facilities') AS element
@@ -11,8 +11,6 @@ SELECT element::text
 	WHERE d.source = 'hagah'
 	GROUP BY element::text;
 
-select element->>'name', element->>'value' from raw_data.data as d, json_array_elements(d.data->'add_info') as element where d.source = 'hagah' group by element->>'name', element->>'value' order by element->>'name';
-
 select count(*) from raw_data.data where source = 'hagah';
 
 select * from raw_data.data where source = 'hagah';
@@ -20,13 +18,22 @@ select * from raw_data.data where source = 'hagah';
 
 delete from raw_data.data where source = 'hagah';
 
-select * from raw_data.data as d where (d.data->>'name')::text = 'Regente Imóveis';
+select * from raw_data.data as d where (d.data->>'name')::text = 'John Bull Pub';
 
-select cat::text from raw_data.data as d, json_array_elements(d.data->'payment_options') as card, json_array_elements(d.data->'categories') as cat where card::text LIKE '%Refeição%' group by cat::text order by cat::text
+--query to select food service categories
+select distinct cat::text from raw_data.data as d, json_array_elements(d.data->'payment_options') as card, json_array_elements(d.data->'categories') as cat where card::text LIKE '%Refeição%' or card::text LIKE '%Alimentação%' order by cat::text;
+
+--query to select facilities of food rubrics
+select regexp_replace(cat::text, '(\(.*\))','') from raw_data.data as d, json_array_elements(d.data->'payment_options') as card, json_array_elements(d.data->'facilities') as cat where card::text LIKE '%Refeição%' or card::text LIKE '%Alimentação%' group by regexp_replace(cat::text, '(\(.*\))','') order by regexp_replace(cat::text, '(\(.*\))','');
+
+--queries to count companies that offer food services
+select distinct count(*) from raw_data.data as d, json_array_elements(d.data->'categories') as cat where cat::text IN ('"Eventos"')
+select distinct count(*) from raw_data.data as d, json_array_elements(d.data->'categories') as cat where cat::text
+	IN (select cat::text from raw_data.data as d, json_array_elements(d.data->'payment_options') as card, json_array_elements(d.data->'categories') as cat where card::text LIKE '%Refeição%' or card::text LIKE '%Alimentação%' group by cat::text order by cat::text)
 
 select (d.data->>'name')::text from raw_data.data as d where source = 'hagah' group by (d.data->>'name')::text HAVING COUNT(*) > 1
 select regexp_replace(element::text, '(\(.*\))','') from raw_data.data as d, json_array_elements(d.data->'facilities') as element where d.source = 'hagah' group by regexp_replace(element::text, '(\(.*\))','');
-select element::text from raw_data.data as d, json_array_elements(d.data->'payment_options') as element where d.source = 'hagah' group by element::text order by element::text;
+select element::text from raw_data.data as d, json_array_elements(d.data->'categories') as element where d.source = 'hagah' group by element::text order by element::text;
 select element->>'name', element->>'value' from raw_data.data as d, json_array_elements(d.data->'add_info') as element where d.source = 'hagah' group by element->>'name', element->>'value' order by element->>'name'
 
 select element::text from raw_data.data as d, json_array_elements(d.data->'categories') as element where d.source = 'hagah' and  group by element::text order by element::text;
