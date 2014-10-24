ce51f2f1a2361c710d13a67b23b320e5693ca735
mbragin <bangvalo@gmail.com>
2014-10-24 14:50:33 -0200

    added parse support of some fields

--- listings-etl/scrapy_crawler/scrapy_crawler/spiders/hagah.py
+++ listings-etl/scrapy_crawler/scrapy_crawler/spiders/hagah.py
@@ -23,13 +23,75 @@ class HagahSpider(CrawlSpider):
     rules = (
         # Extract links matching category list page
         # and follow links from them (since no callback means follow=True by default).
-        Rule(LinkExtractor(allow='.+hagah\.com\.br\/\w+\/\w+\/guia\/$')),
+        Rule(LinkExtractor(allow='.+hagah\.com\.br\/\w+\/.+\/guia\/$')),
 
         # Extract links matching company list (category page) and parse them with the spider's method
         # parse_company_list_page
-        Rule(LinkExtractor(allow='.+hagah\.com\.br\/\w+\/\w+\/guia\/\w+\?q=.+', canonicalize=False, unique=True),
+        Rule(LinkExtractor(allow='.+hagah\.com\.br\/\w+\/.+\/guia\/\w+\?q=.+', canonicalize=False, unique=True),
              callback='parse_company_list_page', follow=True),
+
+        # parse company profile page
+        Rule(LinkExtractor(allow='.+hagah\.com\.br\/\w+\/.+\/local\/.+\.html$', canonicalize=False, unique=True),
+             callback='parse_company_profile_page', follow=False)
     )
+
+    def parse_company_profile_page(self, response):
+        """
+        parses company profile page and returns company profile items
+        :param response:
+        :return:
+        """
+
+        self.log('Found company profile page url %s' % response.url)
+        name = response.css('h1.fn.org::text').extract()
+        if name:
+            name = name[0]
+        else:
+            self.log('Error no name found %s' % response.url)
+            return
+
+        categories = response.css('div.categoriasDetalhe').css('a::text').extract()
+        if categories:
+            categories = [c.lower() for c in categories]
+
+        # a link for google maps to get an address
+        google_maps_link = response.css('a.btn-map.route').xpath('@href').extract()
+        if google_maps_link:
+            google_maps_link = google_maps_link[0]
+
+        url = response.xpath('//*[@id="miolo"]/div[2]/div[3]/div[1]/div[2]/div[2]/ul[1]/li[3]/a/text()').extract()
+        if url:
+            url = [2].css('a::text').extract()
+            if url:
+                url = url[0]
+
+        phones = response.xpath('//*[@id="miolo"]/div[2]/div[4]/div[1]/div[2]/div[3]/ul[1]/li[3]/span/text()').extract()
+        if phones:
+            #todo remove all /n and /t symbols - clear out phone
+            phones = [p for p in phones]
+
+        short_desc = response.xpath('//*[@id="corpo-abas"]/section/article/h3/text()').extract()
+        if short_desc:
+            short_descr = short_desc[0]
+
+        full_desc = response.xpath('//*[@id="corpo-abas"]/section/article/p/text()').extract()
+        if full_desc:
+            full_desc = full_desc[0]
+
+        item_infoes = response.css('#corpo-abas > section > div.itemInfo')
+        if item_infoes:
+            for i in item_infoes:
+                h3 = item_infoes.xpath('h3/text()').extract()
+                if h3:
+                    if 'Horário de atendimento'.lower() in h3[0].lower():
+                        working_hours = response.css('#corpo-abas > section > div.itemInfo')[0].xpath(
+                            'span/text()').extract()
+                        if working_hours:
+                            working_hours = working_hours[0]
+
+
+        attributes = response.css(
+            '#corpo-abas > section > div.itemInfo.caract > ul.listaCaract > li > span::text').extract()
 
     def parse_company_list_page(self, response):
         """
@@ -47,7 +109,7 @@ class HagahSpider(CrawlSpider):
         # get the number of results to build paging urls
         raw_result_num = response.xpath('//span[@class="spanResultado"]/text()').re('\d+')
 
-        #response.xpath('//div[@class="item destacado"]//h2')
+        # response.xpath('//div[@class="item destacado"]//h2')
         # CSS equivalent response.css('div[class="item destacado"] h2')
         #to get selected items
         #name: response.css('div.item.destacado h2 a::text').extract()
